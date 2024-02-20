4e478013809502647532e47e3ca2bf6728cfa746
Ritesh <subh121997@gmail.com>
2024-02-20 22:24:49 +0530

    updates

--- src/test/java/automation/stepDefinitions/StepDefinitionImplementation.java
+++ src/test/java/automation/stepDefinitions/StepDefinitionImplementation.java
@@ -16,7 +16,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitionImplementation extends BaseTest{
-
+//Git branch changes
 	public LandingPage landingPage;
 	public ProductCatalogue productCatalogue;
 	public ConfirmationPage confirmationPage;
